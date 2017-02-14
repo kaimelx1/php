5f81abd195523e8b93aa35a5459234588f5e9f88
Пользователь <>
2017-02-14 16:33:03 +0200

    yii2

---
+++ web/js/script.js
@@ -0,0 +1,21 @@
+/*$(document).ready(function() {
+    $('#user-password').click(function() {
+        if($('#user-password').attr('type') == 'text') {
+            $('#user-password').attr('type', 'password');
+        } else {
+            $('#user-password').attr('type', 'text');
+        }
+    });
+});*/
+
+$(document).ready(function() {
+    $('#user-password').focusin(function() {
+        $('#user-password').attr('type', 'text');
+    });
+});
+
+$(document).ready(function() {
+    $('#user-password').focusout(function() {
+        $('#user-password').attr('type', 'password');
+    });
+});
